f854b236859552bd20c1a632a7032114f339eb1d
at-sacai <1216.sacai@gmail.com>
2024-09-16 15:36:27 +0900

    デフォルト値として空文字を追加

--- db/migrate/20240824125517_add_image_column_items.rb
+++ db/migrate/20240824125517_add_image_column_items.rb
@@ -2,6 +2,6 @@
 
 class AddImageColumnItems < ActiveRecord::Migration[7.0]
   def change
-    add_column :items, :image, :string, null: false
+    add_column :items, :image, :string, null: false, default: ''
   end
 end
