0d8734933e9332f14a7a54c6c9828655cc2d4830
at-sacai <1216.sacai@gmail.com>
2024-09-22 19:14:48 +0900

    不要箇所の削除

--- db/schema.rb
+++ db/schema.rb
@@ -48,15 +48,7 @@ ActiveRecord::Schema[7.0].define(version: 2024_08_24_135506) do
     t.integer "price", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.string "image"
-  end
-
-  create_table "tasks", force: :cascade do |t|
-    t.string "title"
-    t.text "description"
-    t.integer "status"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.string "image", default: ""
   end
 
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
