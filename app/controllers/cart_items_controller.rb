6bad2910f2924cb526f2c99510fe8ebfa9f2d96b
at-sacai <1216.sacai@gmail.com>
2024-09-26 23:49:28 +0900

    current_cart メソッドの追加

--- app/controllers/cart_items_controller.rb
+++ app/controllers/cart_items_controller.rb
@@ -1,10 +1,18 @@
 class CartItemsController < ApplicationController
-  def index
-  end
+  def index; end
 
-  def create
-  end
+  def create; end
 
-  def destroy
+  def destroy; end
+
+  private
+
+  def current_cart
+    if session[:cart_id]
+      @cart = Cart.find(session[:cart_id])
+    else
+      @cart = Cart.create
+      session[:cart_id] = @cart.id
+    end
   end
 end
