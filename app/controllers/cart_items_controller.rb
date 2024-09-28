1ce97bf7e0a523dc9f51538f58f88e6b2fc7e61d
at-sacai <1216.sacai@gmail.com>
2024-09-28 19:51:31 +0900

    Rubocop の auto correct

--- app/controllers/cart_items_controller.rb
+++ app/controllers/cart_items_controller.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 class CartItemsController < ApplicationController
   def index
     @total_price = 0
--- spec/requests/cart_items_spec.rb
+++ spec/requests/cart_items_spec.rb
@@ -1,25 +1,26 @@
+# frozen_string_literal: true
+
 require 'rails_helper'
 
-RSpec.describe "CartItems", type: :request do
-  describe "GET /index" do
-    it "returns http success" do
-      get "/cart_items/index"
+RSpec.describe 'CartItems' do
+  describe 'GET /index' do
+    it 'returns http success' do
+      get '/cart_items/index'
       expect(response).to have_http_status(:success)
     end
   end
 
-  describe "GET /create" do
-    it "returns http success" do
-      get "/cart_items/create"
+  describe 'GET /create' do
+    it 'returns http success' do
+      get '/cart_items/create'
       expect(response).to have_http_status(:success)
     end
   end
 
-  describe "GET /destroy" do
-    it "returns http success" do
-      get "/cart_items/destroy"
+  describe 'GET /destroy' do
+    it 'returns http success' do
+      get '/cart_items/destroy'
       expect(response).to have_http_status(:success)
     end
   end
-
 end
