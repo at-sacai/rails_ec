e995647c317c2d7726df1f696fd023d325bb8bb7
at-sacai <1216.sacai@gmail.com>
2024-09-29 18:10:13 +0900

    quantity 属性の nil チェックを追加

--- app/controllers/cart_items_controller.rb
+++ app/controllers/cart_items_controller.rb
@@ -12,6 +12,7 @@ class CartItemsController < ApplicationController
     quantity = params[:quantity] || 1
 
     @cart_item = @cart.cart_items.find_or_initialize_by(item_id: params[:id])
+    @cart_item.quantity ||= 0
     @cart_item.quantity += quantity.to_i
 
     if @cart_item.save
