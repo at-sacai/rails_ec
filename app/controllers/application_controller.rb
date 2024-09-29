6feaab20ab5a5970cc759934a4ad6bd04c5a3200
at-sacai <1216.sacai@gmail.com>
2024-09-29 17:49:07 +0900

    private の追加

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,6 +4,8 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   before_action :set_cart
 
+  private
+
   def set_cart
     unless session[:cart_id]
       @cart = Cart.create
