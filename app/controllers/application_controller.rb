d74bcef379d5878355fdd572cf47bc72835ecef4
at-sacai <1216.sacai@gmail.com>
2024-09-29 17:51:13 +0900

    set_cart メソッドのリファクタリング

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,10 +7,7 @@ class ApplicationController < ActionController::Base
   private
 
   def set_cart
-    unless session[:cart_id]
-      @cart = Cart.create
-      session[:cart_id] = @cart.id
-    end
-    @cart = Cart.find_by(id: session[:cart_id])
+    @cart = Cart.find_or_create_by(id: session[:cart_id])
+    session[:cart_id] ||= @cart.id
   end
 end
