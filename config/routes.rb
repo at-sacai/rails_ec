bc4f2e97c0d3a2b575bb2903fb3c625b3506c860
at-sacai <1216.sacai@gmail.com>
2024-10-05 20:26:20 +0900

    orders コントローラーに対するルーティングの修正

--- config/routes.rb
+++ config/routes.rb
@@ -4,14 +4,13 @@ Rails.application.routes.draw do
   get 'cart' => 'cart_items#index'
   post 'cart/:id/' => 'cart_items#create'
   delete 'cart/:id/' => 'cart_items#destroy'
+  resources :orders
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
   root 'items#index'
   resources :items, only: %i[index create show]
-  resources :orders, only: %i[create]
   namespace :admin do
     resources :items, only: %i[index new create edit update destroy]
-    resources :orders, only: %i[index]
   end
 end
