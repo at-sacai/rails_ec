67ac44ae2a0b008eba1a83c012728682c3c4b9c5
at-sacai <1216.sacai@gmail.com>
2024-10-05 18:07:33 +0900

    total_price カラムのデータ型の修正

--- db/migrate/20240930155042_create_orders.rb
+++ db/migrate/20240930155042_create_orders.rb
@@ -3,12 +3,12 @@ class CreateOrders < ActiveRecord::Migration[7.0]
     create_table :orders do |t|
       t.string :first_name, null: false
       t.string :last_name, null: false
-      t.string :email, null: false, format: { with: /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i }
-      t.string :address, null: false, uniq: true
+      t.string :email, null: false, uniq: true
+      t.string :address, null: false
       t.string :address_2
       t.string :country, null: false
       t.string :state, null: false
-      t.string :zip, null: false, format: { with: /^\d{3}[-]\d{4}$|^\d{3}[-]\d{2}$|^\d{3}$|^\d{5}$|^\d{7}$/s }
+      t.string :zip, null: false
       t.integer :total_price, null: false
 
       t.timestamps
