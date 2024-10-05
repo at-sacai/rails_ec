42f0116aed70354f7cb9d6a02770db2a4be1d480
at-sacai <1216.sacai@gmail.com>
2024-10-05 17:28:01 +0900

    マイグレーションファイルの修正

--- db/migrate/20240930155405_create_order_items.rb
+++ db/migrate/20240930155405_create_order_items.rb
@@ -1,9 +1,9 @@
 class CreateOrderItems < ActiveRecord::Migration[7.0]
   def change
     create_table :order_items do |t|
-      t.integer :quantity, null: false, presence: true, numericality: true
       t.references :order, null: false, foreign_key: true
       t.references :item, null: false, foreign_key: true
+      t.integer :quantity, null: false
 
       t.timestamps
     end
