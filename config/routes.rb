63309958a470a3d0ead853aff296595938aba77b
at-sacai <1216.sacai@gmail.com>
2024-10-06 15:16:16 +0900

    購入履歴閲覧機能の作成

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,7 @@ Rails.application.routes.draw do
   get 'cart' => 'cart_items#index'
   post 'cart/:id/' => 'cart_items#create'
   delete 'cart/:id/' => 'cart_items#destroy'
-  resources :orders
+  resources :orders, only: %i[create]
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
@@ -12,5 +12,6 @@ Rails.application.routes.draw do
   resources :items, only: %i[index create show]
   namespace :admin do
     resources :items, only: %i[index new create edit update destroy]
+    resources :orders, only: %i[index]
   end
 end
