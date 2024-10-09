61cdd87085498edb63bcbecaffa40bb3aae119a6
at-sacai <1216.sacai@gmail.com>
2024-10-10 08:26:02 +0900

    購入処理の処理順の修正

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -6,11 +6,10 @@ class OrdersController < ApplicationController
     @order.total_price = @cart.cart_items.sum(&:calculate_price)
 
     ApplicationRecord.transaction do
+      @cart.cart_items.each do |cart_item|
+        order_item = @order.order_items.build(item_id: cart_item.item_id, quantity: cart_item.quantity)
+      end
       if @order.save
-        @cart.cart_items.each do |cart_item|
-          order_item = @order.order_items.build(item_id: cart_item.item_id, quantity: cart_item.quantity)
-          order_item.save
-        end
         PurchaseMailer.with(order: @order).thanks_email.deliver_now
         flash[:notice] = 'Purchase Complete.'
         clear_cart
