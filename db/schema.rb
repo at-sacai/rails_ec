82ee850380e1be15156fcdf73c50a3e4e2b10220
at-sacai <1216.sacai@gmail.com>
2024-10-11 01:43:44 +0900

    coponsテーブルの追加

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2024_09_30_155405) do
+ActiveRecord::Schema[7.0].define(version: 2024_10_10_162921) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -53,6 +53,13 @@ ActiveRecord::Schema[7.0].define(version: 2024_09_30_155405) do
   end
 
   create_table "carts", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "coupons", force: :cascade do |t|
+    t.string "code", null: false
+    t.integer "discount_price", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
