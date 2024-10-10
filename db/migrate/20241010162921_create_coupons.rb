64d7d45da6acae2669bc748e80ee291b3e8e8f30
at-sacai <1216.sacai@gmail.com>
2024-10-11 01:42:43 +0900

    code カラムにユニークキー制約の追加

--- db/migrate/20241010162921_create_coupons.rb
+++ db/migrate/20241010162921_create_coupons.rb
@@ -1,10 +1,11 @@
 class CreateCoupons < ActiveRecord::Migration[7.0]
   def change
     create_table :coupons do |t|
-      t.string :code, null: false, unique: true
+      t.string :code, null: false
       t.integer :discount_price, null: false
 
       t.timestamps
     end
+    add_index :coupons, [:code], unique: true
   end
 end
