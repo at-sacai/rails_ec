8fac256f3cfbfa5b06f4bb5134ed0cae0d1f5db0
at-sacai <1216.sacai@gmail.com>
2024-10-12 14:39:47 +0900

    タイポの修正

--- app/models/coupon.rb
+++ app/models/coupon.rb
@@ -1,5 +1,5 @@
 class Coupon < ApplicationRecord
   belongs_to :order
-  valiadate :code, uniqueness: true, length: { is: 7 }
+  validaate :code, uniqueness: true, length: { is: 7 }
   validates :discount_price, numericality: true
 end
