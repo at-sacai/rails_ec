fb206941ecc43187d2e10ebfcdd7d9658d032d10
at-sacai <1216.sacai@gmail.com>
2024-10-12 16:03:37 +0900

    coupons テーブルに、order_id カラムを追加

--- db/migrate/20241010162921_create_coupons.rb
+++ db/migrate/20241010162921_create_coupons.rb
@@ -1,6 +1,7 @@
 class CreateCoupons < ActiveRecord::Migration[7.0]
   def change
     create_table :coupons do |t|
+      t.references :order, foreign_key: true
       t.string :code, null: false
       t.integer :discount_price, null: false
 
